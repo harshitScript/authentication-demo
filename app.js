4448d3550a355f66d5210dc9732cb79d81c44b07
harshitScript <85029089+harshitScript@users.noreply.github.com>
2022-09-04 18:00:14 +0530

    standard js style fixed

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const { config } = require("dotenv");
+const express = require('express')
+const { config } = require('dotenv')
 
-config();
-const server = express();
+config()
+const server = express()
 
 server.use((req, res, next) => {
-  return res.send({ message: "We are coming soon." });
-});
+  return res.send({ message: 'We are coming soon.' })
+})
 
 server.listen(process.env.PORT, (err) => {
-  if (err)
+  if (err) {
     return console.log(
       `Error occurred while starting the port ${process.env.PORT}`
-    );
+    )
+  }
 
-  return console.log(`Server started at port ${process.env.PORT}`);
-});
+  return console.log(`Server started at port ${process.env.PORT}`)
+})
